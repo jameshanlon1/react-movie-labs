b71d5a040eb755361accb876d191c0018194f212
james <jameshanlonrcs@gmail.com>
2024-10-22 20:25:12 +0100

    Added Upcoming movies page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -10,6 +10,7 @@ import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import Upcoming from "./pages/upcoming";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,6 +34,7 @@ const App = () => {
             <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
+            <Route path="/movies/upcoming" element={<Upcoming />} />
             <Route path="*" element={ <Navigate to="/" /> } />
           </Routes>
         </MoviesContextProvider>
